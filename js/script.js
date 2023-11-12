eb3f8abbb50dd8fd70b2b1a5900d583d0828fab8
QuisbertLaura <quisbertlaura007@gmail.com>
2023-11-12 15:16:07 -0400

    Agregando estilos y contenido del index

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 const navBar = document.getElementById("navBar");
-const blockNavBarSecundary = document.getElementById(".navBarSecundary");
+const blockNavBarSecundary = document.getElementById("navBarSecundary");
 
-navBar.addEventListener('mousemove', () => {
-    
-})
+navBar.addEventListener('mouseenter', () => {
+    blockNavBarSecundary.style.display = "block";
+});
+
+navBar.addEventListener('mouseout', () => {
+    blockNavBarSecundary.style.display = "none";
+});
